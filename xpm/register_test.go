fa8a0d42edd033afe1969945dd2e9c3989950496
Andy Williams <andy@andy.xyz>
2022-07-18 08:37:25 -0700

    Add XPM support to image library

---
+++ xpm/register_test.go
@@ -0,0 +1,25 @@
+package icon_test
+
+import (
+	"image"
+	_ "image/png"
+	"os"
+	"testing"
+
+	_ "github.com/fyne-io/image/xpm" // xpm image parser
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRegister_Read(t *testing.T) {
+	r, err := os.Open("testdata/blarg.xpm")
+	assert.Nil(t, err)
+	img, fmt, err := image.Decode(r)
+	assert.Nil(t, err)
+	assert.Equal(t, "xpm", fmt)
+
+	assert.Equal(t, 0, img.Bounds().Min.X)
+	assert.Equal(t, 0, img.Bounds().Min.Y)
+	assert.Equal(t, 16, img.Bounds().Max.X)
+	assert.Equal(t, 7, img.Bounds().Max.Y)
+}
